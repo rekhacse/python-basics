bec3fe0bb6b3f4280420d4578b941a82ca992f87
rekha <rekhagitcse@gmail.com>
2014-01-27 17:10:21 +0530

    Create list.py
    list and tuples.

---
+++ list.py
@@ -0,0 +1,14 @@
+a=[10,20,30,40,60]
+print(a[0])
+b=['a','b','c']
+print(b[1])
+del (a[0])
+# delete the values in the list
+a.insert(5,87)
+for i in a:
+ print (b)
+print(len(a))
+c=("a","b","def")
+print(c)
+#these are tuples. we cant add elements to it
+print(a)
