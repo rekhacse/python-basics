cdfe7ac1ecb19e3318d7d1a5aa10c83ecf8e14dc
rekha <rekhagitcse@gmail.com>
2014-01-27 16:39:19 +0530

    Create listform.py
    creates a list

---
+++ listform.py
@@ -0,0 +1,5 @@
+a=[10,20,30,40,60]
+print(a[0])
+b=['a','b','c']
+print(b[1])
+#list can contains either number or string only.string must be given in strings. both cannot be defined in the same list.
