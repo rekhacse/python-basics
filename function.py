33d50c459c2aeac7500976f6a32033a7c6d20fda
rekha <rekhagitcse@gmail.com>
2014-01-27 17:38:38 +0530

    Create function.py
    here def means defining a function

---
+++ function.py
@@ -0,0 +1,10 @@
+#function
+def add(a,b):
+ return a+b
+print(add(23,34))
+
+def sub():
+ i=int(input("Enter the value of a:"))	
+ j=int(input("Enter the value of b:"))
+ return i-j
+print(sub())			
